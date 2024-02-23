7156f9dbfbfd41d7a62a3112c2f4eb5f9c8226ae
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-02-23 19:23:32 +0900

    random.rb修正（各メンバーセットにsampleを用いて初期配列のshuffleを削除）

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,17 +1,15 @@
 class RandomGroup
   # 振り分け用のメンバー配列を用意
-  members = [*'A'..'F'].shuffle
+  members = [*'A'..'F']
 
   # グループ分けの数を決定する
-  group_a = Array.new(rand((2..4)))
+  group_a = Array.new(rand(2..4))
 
   # グループAに各メンバーの振り分けを実施
-  group_a.length.times do |i|
-    group_a[i] = members[i] if group_a.length > i
-  end
+  group_a[group_a.index(nil)] = members.delete(members.sample) while group_a.include?(nil)
 
   # グループBに残りのメンバーをセット
-  group_b = members - group_a
+  group_b = members
 
   # 結果出力
   print "#{group_a.sort}\n#{group_b.sort}"
