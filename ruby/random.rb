6b9672fa9136bb1df9939343b546a0fca4710e06
github akari07voi <ymytxxo29@yahoo.co.jp>
2024-02-25 10:47:56 +0900

    ramdom.rb修正（group_aのセット時にsampleの引数を追加、他余分な記述削除）

--- ruby/random.rb
+++ ruby/random.rb
@@ -2,14 +2,11 @@ class RandomGroup
   # 振り分け用のメンバー配列を用意
   members = [*'A'..'F']
 
-  # グループ分けの数を決定する
-  group_a = Array.new(rand(2..4))
-
   # グループAに各メンバーの振り分けを実施
-  group_a[group_a.index(nil)] = members.delete(members.sample) while group_a.include?(nil)
+  group_a = members.sample(rand(2..4))
 
   # グループBに残りのメンバーをセット
-  group_b = members
+  group_b = members - group_a
 
   # 結果出力
   print "#{group_a.sort}\n#{group_b.sort}"
